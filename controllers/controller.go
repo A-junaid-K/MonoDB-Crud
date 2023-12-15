19ed06cc5323fb6ad354e1fb36101117ef0f8f32
Abdullah junaid <junaidkaidakath@gmail.com>
2023-12-15 17:42:22 +0530

    delete & delete Many

--- controllers/controller.go
+++ controllers/controller.go
@@ -18,13 +18,33 @@ func insertMovie(movie model.Netfilx) {
 	}
 	fmt.Println("Inserted one movei in database with id : ", insert.InsertedID)
 }
-func updatemovie(Id string){
-	movieId,_ := primitive.ObjectIDFromHex(Id)
-	filter := bson.M{"_id":movieId}
-	update := bson.M{"$set":bson.M{"watched":true}}
-	result , err := database.Collection.UpdateOne(context.Background(),filter,update)
+
+// update a record
+func updatemovie(Id string) {
+	movieId, _ := primitive.ObjectIDFromHex(Id)
+	filter := bson.M{"_id": movieId}
+	update := bson.M{"$set": bson.M{"watched": true}}
+	result, err := database.Collection.UpdateOne(context.Background(), filter, update)
 	if err != nil {
-		log.Fatal("failed to update movie : ",err)
+		log.Fatal("failed to update movie : ", err)
 	}
-	fmt.Println("Successfully updated movie \n modified count : ",result.ModifiedCount)
+	fmt.Println("Successfully updated movie \n modified count : ", result.ModifiedCount)
+}
+
+// delete one record from DB
+func deletemovie(Id string) {
+	movieId, _ := primitive.ObjectIDFromHex(Id)
+	filter := bson.M{"_id": movieId}
+	deletecount, _ := database.Collection.DeleteOne(context.Background(), filter)
+	fmt.Println("Movie successfully deleted with delete count : ", deletecount)
+}
+
+// delte multiple record from the database
+func deleteMany(Id string) int64{
+	result, err := database.Collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+	if err != nil {
+		log.Fatal("Failed to dalate all records from the Db : ", err)
+	}
+	fmt.Println("Succeffully deleted all records with delet count : ", result.DeletedCount)
+	return result.DeletedCount
 }
